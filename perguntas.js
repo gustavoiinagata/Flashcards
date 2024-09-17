e3861e17d929cd4690f23584d6e7bdbe3d8487ee
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-09-17 08:02:11 -0300

    n

---
+++ perguntas.js
@@ -0,0 +1,8 @@
+criaCartao(
+    ‘Matemática’
+    ‘O que é função afim?’,
+    ‘A função afim é qualquer função que possua a lei de formação y = ax + b, sendo a e b números reais e a diferente de zero.’)
+    
+    criaCartao(
+    ‘
+    
