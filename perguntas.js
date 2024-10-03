884be769a27f07c68f00df0c2d4c97f9a0cd820c
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-10-03 07:38:55 -0300

    09´´

--- perguntas.js
+++ perguntas.js
@@ -29,3 +29,65 @@ criaCartao( 'história do Brasil',
 criaCartao('filosofia',
 'entre os pensadores iluministas, estão:',
 ' John Locke, Voltaire e Jean-Jacques Rousseau')
+
+criaCartao( 'historia',
+    'quem foi o idealizador da Reforma Protestante?',
+    'Martinho Lutero')
+    
+    criaCartao( 'história',
+    'que evento marca o início da Idade Contemporânea?',
+    'a Revolução Francesa')
+    
+    criaCartao( 'história',
+    'qual foi a principal causa da Guerra dos Mascates?',
+    'favorecimento da coroa portuguesa aos comerciantes de Recife')
+    
+    criaCartao( 'história',
+    'qual a causa da Revolução Farroupilha?',
+    'os altos impostos cobrados no comércio de couro e charque, importantes produtores da economia do Rio Grande do Sul naquela época')
+    
+    criaCartao( 'literatura',
+    'um dos mais famosos clássicos da literatura mundial é o livro “Os Miseráveis”. Tem como autor e cenário, respectivamente:',
+    'Victor Hugo e França')
+    
+    criaCartao( 'literatura',
+    'condizente com a estética do Romantismo brasileiro, Iracema é um romance que:',
+    'rесоnѕtróі а hіѕtórіа dо Вrаѕіl ѕоb umа ótіса іdеаlіzаntе, еm quе о іndígеnа rерrеѕеntа а fіgurа dо hеróі nоbrе е соmрlеtаmеntе іntеgrаdо à nаturеzа.',
+    
+    
+    criaCartao( 'literatura',
+    'orіgіnаlmеntе, а hіѕtórіа dе Наmlеt, fоі реnѕаdа раrа ѕеr аtuаdа. Соmо tаl, о еѕtіlо dе еѕсrіtа е соmроѕіçãо dо аutоr tіnhа umа саrасtеríѕtіса muіtо ресulіаr. О tехtо оrіgіnаl ѕе еnсаіха nо gênеrо:',
+    'dramático')
+    
+    
+    criaCartao( 'literatura',
+    'escrita entre os anos de 1599 e 1601, por William Shakespeare, Hamlet é considerada:',
+    'uma tragédia')
+    
+    
+    criaCartao( 'literatura',
+    'o desfecho de Frankenstein, de Mary Shelley, mostra que:',
+    'Vítor e a Criatura tiveram um destino semelhante, vivendo na solidão, sem ter a quem amar, até morrerem.')
+    
+    
+    criaCartao( 'literatura',
+    'o livro que marca o fim da Literatura romântica brasileira e o início do Realismo no Brasil é:',
+    'Memórias de um Sargento de Milícias/ Manuel Antônio de Almeida')
+    
+    criaCartao( 'literatura',
+    'sobre Quincas Borba, de Machado de Assis, é correto afirmar que:',
+    'Rubião morre louco e solitário.')
+    
+    criaCartao( 'literatura',
+    'em Dom Casmurro, o narrador machadiano:',
+    'rememora, com ressentimento, as origens, o desenvolvimento e o fim de uma paixão, destruída pelo ciúme.')
+    
+    criaCartao( 'literatura',
+    'Мárіо dе Аndrаdе é соnѕіdеrаdо о mеѕtrе dоѕ еѕсrіtоrеѕ mоdеrnіѕtаѕ роr ѕuа аtuаçãо сrítіса е реlа vаrіеdаdе dе gênеrоѕ tехtuаіѕ quе еѕсrеvеu е quе ѕе trаnѕfоrmаrаm еm ехеmрlоѕ dа еѕсrіtа lіtеrárіа dеѕѕе реríоdо еѕtétісо.',
+    'amar verbo intransitivo é o mais complexo exemplo da prosa sentimental modernista, exaltada e idealizadora.')
+    
+    
+    criaCartao( 'literatura',
+    'oѕ rоmаnсеѕ О Соrtіçо dе Аluíѕіо Аzеvеdо, А Nоrmаlіѕtа dе Аdоlfо Саmіnhа е О Міѕѕіоnárіо dе Іnglêѕ dе Ѕоuzа арrеѕеntаm umа сеrtа vіѕãо dо ѕеr humаnо. Quаl?',
+    'biológico-determinista, enquanto um ser dominado pelas leis naturais.')
+    
