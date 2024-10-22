256b650fcedcf50f33aff28b8d92dad52cadbea1
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-10-22 10:39:29 -0300

    dedv

---
+++ projeto2.js
@@ -0,0 +1,28 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class = "cartao-conteudo" onclick="mostraResposta (this)>
+<h3> ${categoria}</h3>
+<div class = "cartao-pergunta">
+<p> ${pergunta}</p>
+</div>
+<div class = "cartao-resposta">
+<p> ${resposta}</p>
+</div>
+</div>
+`;
+
+container.appendChild(cartao);
+}
+
+function mostraResposta(card){
+const resposta= card.querySelector('.cartap-reposta');
+if(resposta.style.display === 'none'){
+    resposta.style.display = 'block';
+}
+else{
+    resposta.style.display = 'none';
+ }
+}
